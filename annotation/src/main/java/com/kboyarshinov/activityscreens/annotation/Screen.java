462f34c8ca6acc5ffa3c74f0e00d86fc59a8c0b1
Kirill Boyarshinov <naghtarr@gmail.com>
2015-01-16 14:23:27 +0600

    Initial setup

---
+++ annotation/src/main/java/com/kboyarshinov/activityscreens/annotation/Screen.java
@@ -0,0 +1,16 @@
+package com.kboyarshinov.activityscreens.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to indicate classes that are screens
+ *
+ * @author Kirill Boyarshinov
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface Screen {
+}
---
+++ processor/src/main/java/com/kboyarshinov/activityscreen/processor/ActivityScreenProcessor.java
@@ -0,0 +1,51 @@
+package com.kboyarshinov.activityscreen.processor;
+
+import com.google.auto.service.AutoService;
+import com.kboyarshinov.activityscreens.annotation.Screen;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Annotation Processor for @Screen annotation
+ *
+ * @author Kirill Boyarshinov
+ */
+@AutoService(Processor.class)
+public class ActivityScreenProcessor extends AbstractProcessor {
+    private Types typeUtils;
+    private Elements elementUtils;
+    private Filer filer;
+    private Messager messager;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        typeUtils = processingEnv.getTypeUtils();
+        elementUtils = processingEnv.getElementUtils();
+        filer = processingEnv.getFiler();
+        messager = processingEnv.getMessager();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new LinkedHashSet<String>();
+        annotations.add(Screen.class.getCanonicalName());
+        return annotations;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
